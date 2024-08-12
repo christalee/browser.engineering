c7e98a048f57e163ee9143819ed823e00c4ff09a
Christalee Bieber <cbieber@alum.mit.edu>
2024-08-12 16:23:25 -0400

    refactoring single-letter vars; adding FONTS and MEASURES caches

--- layout.py
+++ layout.py
@@ -1,8 +1,33 @@
+import tkinter as tk
 from typing import List, Union, Literal
-from tkinter import font
+from tkinter import font as tkfont
 
 HSTEP, VSTEP = 13, 18
 SCROLLBAR_WIDTH = 12
+FONTS = {}
+MEASURES = {}
+
+
+def get_font(size, weight, style):
+  key = (size, weight, style)
+  if key not in FONTS:
+    font = tkfont.Font(
+      size=size,
+      weight=weight,
+      slant=style
+    )
+    label = tk.Label(font=font)
+    FONTS[key] = (font, label)
+  return FONTS[key][0]
+
+
+def get_measure(s, size, weight, style):
+  key = (s, size, weight, style)
+  font = get_font(size, weight, style)
+  if key not in MEASURES:
+    measure = font.measure(s)
+    MEASURES[key] = measure
+  return MEASURES[key]
 
 
 class Text:
@@ -35,61 +60,57 @@ class Layout:
     self.size: int = 16
     self.weight: Literal['normal', 'bold'] = "normal"
     self.style: Literal['roman', 'italic'] = "roman"
-    for t in tokens:
-      self.token(t)
+    for tok in tokens:
+      self.token(tok)
     self.flush()
 
-  def token(self, t):
-    if isinstance(t, str): \
-      self.word(t)
-    elif isinstance(t, Text):
-      for w in t.text.split(" "):
-        self.word(w)
-    elif t.tag == "i":
+  def token(self, tok):
+    if isinstance(tok, str): \
+      self.word(tok)
+    elif isinstance(tok, Text):
+      for word in tok.text.split(" "):
+        self.word(word)
+    elif tok.tag == "i":
       self.style = "italic"
-    elif t.tag == "/i":
+    elif tok.tag == "/i":
       self.style = "roman"
-    elif t.tag == "b":
+    elif tok.tag == "b":
       self.weight = "bold"
-    elif t.tag == "/b":
+    elif tok.tag == "/b":
       self.weight = "normal"
-    elif t.tag == "small":
+    elif tok.tag == "small":
       self.size -= 2
-    elif t.tag == "/small":
+    elif tok.tag == "/small":
       self.size += 2
-    elif t.tag == "big":
+    elif tok.tag == "big":
       self.size += 4
-    elif t.tag == "/big":
+    elif tok.tag == "/big":
       self.size -= 4
-    elif t.tag == "br" or t.tag == "br /":
+    elif tok.tag == "br" or tok.tag == "br /":
       self.flush()
-    elif t.tag == "/p":
+    elif tok.tag == "/p":
       self.flush()
 
-  def word(self, w):
-    f = font.Font(
-      size=self.size,
-      weight=self.weight,
-      slant=self.style
-    )
-    if '\n' in w:
-      newlines = w.split('\n')
+  def word(self, word):
+    font = get_font(self.size, self.weight, self.style)
+    if '\n' in word:
+      newlines = word.split('\n')
       for w in newlines[:-1]:
-        self.line.append((self.cursor_x, w, f))
+        self.line.append((self.cursor_x, w, font))
         self.flush()
       last = newlines[-1]
       if last:
-        self.display_word(last, f)
+        self.display_word(last, font)
     else:
-      self.display_word(w, f)
+      self.display_word(word, font)
 
-  def display_word(self, w, f):
-    space = f.measure(" ")
-    width = f.measure(w)
+  def display_word(self, word, font):
+    space = get_measure(" ", self.size, self.weight, self.style)
+    width = get_measure(word, self.size, self.weight, self.style)
     if self.rtl:
       if self.cursor_x - (width + space) > 0:
         # If there's still room on this line, add to self.line and advance cursor_x
-        self.line.append((self.cursor_x, w, f))
+        self.line.append((self.cursor_x, word, font))
         self.cursor_x -= (width + space)
       else:
         # Otherwise, finish this line
@@ -97,7 +118,7 @@ class Layout:
     else:
       if self.cursor_x + width + space < self.screen_width - SCROLLBAR_WIDTH:
         # If there's still room on this line, add to self.line and advance cursor_x
-        self.line.append((self.cursor_x, w, f))
+        self.line.append((self.cursor_x, word, font))
         self.cursor_x += width + space
       else:
         # Otherwise, finish this line
@@ -106,12 +127,12 @@ class Layout:
   def flush(self):
     if not self.line:
       return
-    metrics = [f.metrics() for x, w, f in self.line]
+    metrics = [font.metrics() for x, word, font in self.line]
     max_ascent = max([metric["ascent"] for metric in metrics])
     baseline = self.cursor_y + 1.25 * max_ascent
-    for x, w, f in self.line:
-      y = baseline - f.metrics("ascent")
-      self.display_list.append((x, y, w, f))
+    for x, word, font in self.line:
+      y = baseline - font.metrics("ascent")
+      self.display_list.append((x, y, word, font))
     max_descent = max([metric['descent'] for metric in metrics])
     self.cursor_y = baseline + 1.25 * max_descent
     if self.rtl:
