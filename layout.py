de5533212a4aeb2bf74be944aa24a329a0981e1c
Christalee Bieber <cbieber@alum.mit.edu>
2024-08-13 12:09:32 -0400

    exercise 3-2 superscript, also debugging soft hyphens

--- layout.py
+++ layout.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from typing import List, Union, Literal
+from typing import List, Union, Literal, Dict
 from tkinter import font as tkfont
 
 HSTEP, VSTEP = 13, 18
@@ -49,12 +49,13 @@ class Tag:
 class Layout:
   def __init__(self, tokens: List[Union[Tag, Text]], screen_width: int):
     self.centering = False
+    self.superscript = False
     self.screen_width = screen_width
     self.line = []
     self.display_list = []
     self.cursor_x = HSTEP
     self.cursor_y: int = VSTEP
-    self.size: int = 16
+    self.size: Union[int, float] = 16
     self.weight: Literal['normal', 'bold'] = "normal"
     self.style: Literal['roman', 'italic'] = "roman"
     for tok in tokens:
@@ -87,10 +88,19 @@ class Layout:
       self.flush()
     elif tok.tag == "/p":
       self.flush()
-    elif tok.tag == 'h1 class="title"':
-      self.centering = True
+    elif tok.tag.startswith('h1'):
+      self.size = self.size * 1.5
+      if 'class="title"' in tok.tag:
+        self.centering = True
     elif tok.tag == "/h1":
+      self.size = self.size / 1.5
       self.centering = False
+    elif tok.tag == "sup":
+      self.size = self.size / 2
+      self.superscript = True
+    elif tok.tag == "/sup":
+      self.size = self.size * 2
+      self.superscript = False
 
   def word(self, word):
     if '\n' in word:
@@ -101,7 +111,8 @@ class Layout:
                           "size": self.size,
                           "weight": self.weight,
                           "style": self.style,
-                          "centering": self.centering})
+                          "centering": self.centering,
+                          "superscript": self.superscript})
         self.flush()
       last = newlines[-1]
       if last:
@@ -119,42 +130,56 @@ class Layout:
                         "size": self.size,
                         "weight": self.weight,
                         "style": self.style,
-                        "centering": self.centering})
+                        "centering": self.centering,
+                        "superscript": self.superscript
+                        })
       self.cursor_x += width + space
     else:
       if u"\u00AD" in word:
         parts = word.split(u"\u00AD")
         hyphen = get_measure("-", self.size, self.weight, self.style)
-        leftovers = ''
+        taken = []
         for part in parts:
           part_width = get_measure(part, self.size, self.weight, self.style)
           if self.cursor_x + part_width + hyphen < self.screen_width - SCROLLBAR_WIDTH:
-            self.line.append({
-              "x": self.cursor_x,
-              "word": part,
-              "size": self.size,
-              "weight": self.weight,
-              "style": self.style,
-              "centering": self.centering
-            })
+            self.line.append({"x": self.cursor_x,
+                              "word": part,
+                              "size": self.size,
+                              "weight": self.weight,
+                              "style": self.style,
+                              "centering": self.centering,
+                              "superscript": self.superscript
+                              })
             self.cursor_x += part_width
+            taken.append(part)
           else:
-            leftovers += part
+            # We've fit all the parts we can fit onto the line (plus hyphen) so break
+            break
 
+        leftovers = ''.join([p for p in parts if p not in taken])
         self.line.append({
           "x": self.cursor_x,
           "word": "-",
           "size": self.size,
           "weight": self.weight,
           "style": self.style,
-          "centering": self.centering
+          "centering": self.centering,
+          "superscript": self.superscript
         })
-        self.flush(leftovers)
+        nextline = {
+          "word": leftovers,
+          "size": self.size,
+          "weight": self.weight,
+          "style": self.style,
+          "centering": self.centering,
+          "superscript": self.superscript
+        }
+        self.flush(nextline)
       else:
         # Otherwise, finish this line
         self.flush()
 
-  def flush(self, nextline: str = ''):
+  def flush(self, nextline: Dict[str, Union[int, str]] = None):
     if not self.line:
       return
 
@@ -174,7 +199,10 @@ class Layout:
 
     for i, entry in enumerate(self.line):
       font = fonts[i]
-      y = baseline - font.metrics("ascent")
+      if entry["superscript"]:
+        y = baseline - max_ascent
+      else:
+        y = baseline - font.metrics("ascent")
       if centering:
         self.display_list.append((entry['x'] + delta_x, y, entry['word'], font))
       else:
@@ -183,15 +211,11 @@ class Layout:
     self.cursor_y = baseline + 1.25 * max_descent
     self.cursor_x = HSTEP
     if nextline:
-      self.line = [{
-        "x": self.cursor_x,
-        "word": nextline,
-        "size": self.size,
-        "weight": self.weight,
-        "style": self.style,
-        "centering": self.centering
-      }]
-      self.cursor_x += get_measure(nextline, self.size, self.weight, self.style) + get_measure(" ", self.size,
-                                                                                               self.weight, self.style)
+      nextline["x"] = self.cursor_x
+      self.line = [nextline]
+      self.cursor_x += get_measure(nextline["word"] + " ",
+                                   nextline["size"],
+                                   nextline["weight"],
+                                   nextline["style"])
     else:
       self.line = []
